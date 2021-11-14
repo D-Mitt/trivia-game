38a7bd0095768c6f1b84e84ca2829289faa59e1d
Daniel Mitton <blank>
2021-11-13 21:13:10 -0400

    updaing build scripts v1

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-import express from 'express';
+const express = require('express');
 const app = express()
 const port = 5000
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
+  console.log(`Production ready`)
   app.use(express.static("client/build"));
 }
 
-app.listen(port, () => {
-  console.log(`Server listening on port: ${port}`)
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server started!`)
 })
